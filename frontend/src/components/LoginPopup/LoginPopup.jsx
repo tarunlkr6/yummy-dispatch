e499003f6747659d2f94c5b0de52225523e7de00
Sukh767 <sukhranjanjana767@gmail.com>
2024-10-06 23:49:45 +0530

    Add frontend folder

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginPopup.css'
+
+const LoginPopup = () => {
+  return (
+    <div className='login-popup'>
+      
+    </div>
+  )
+}
+
+export default LoginPopup
---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Cart.css'
+
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
