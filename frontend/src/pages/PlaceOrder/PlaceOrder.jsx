5678172e30da3595829ce3ad84ef36ec69e4aa78
Sukh767 <sukhranjanjana767@gmail.com>
2024-10-09 23:00:26 +0530

    Implement Place Order page with routing, responsive design, and functional Place Order button and fix footer section

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
+import 'boxicons'
+
 
 const PlaceOrder = () => {
+
+  const { getTotalCartAmount } = useContext(StoreContext)
+
   return (
-    <div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name'/>
+          <input type="text" placeholder='Last Name'/>
+        </div>
+        <input type="email" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip Code'/>
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-totals-details">
+              <p>Subtotal</p>
+              <p>&#36;{getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+            <div className="cart-totals-details">
+                <p>Delivery Fee</p>
+                <p>&#36;{getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr/>
+            <div className="cart-totals-details">
+                <b>Total</b>
+                <b>&#36;{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount()  + 2}</b>
+            </div>
+          </div>
+          <button>Proceed To Payment</button>
+        </div>
+      </div>
       
-    </div>
+    </form>
   )
 }
 
