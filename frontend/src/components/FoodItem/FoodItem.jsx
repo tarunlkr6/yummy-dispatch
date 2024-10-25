d4a9db3490c971bd17d16e06d4665f1ead391d09
Sukh767 <sukhranjanjana767@gmail.com>
2024-10-25 16:28:15 +0530

    Implement backend data fetching to display food list

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react'
 import './FoodItem.css'
-import { assets } from '../../../assets/assets'
-import { StoreContext } from '../../../context/StoreContext'
+import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
 
     // const [itemCount,setItemCount] = useState(0)
-    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img className='food-item-image' src={image} alt="" />
+                <img className='food-item-image' src={url+'/images/'+image} alt="" />
                 {!cartItems[id]
                     ? <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
                     : <div className='food-item-counter'>
